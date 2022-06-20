565dc917c636795dcff0563091d4713f77b7b79c
saurabh <saurabh.waghchoure2805@gmail.com>
2022-06-20 16:05:48 +0530

    multi-added

--- page1.js
+++ page1.js
@@ -3,7 +3,7 @@ function add(a, b) {
   console.log(`${a} + ${b} = ${a + b}`)
 }
 add(10, 20)
-function multi(a, b) {
-  console.log(`${a} * ${b} = ${a + b}`)
+function div(a, b) {
+  console.log(`${a} /${b} = ${a + b}`)
 }
 multi(10, 20)
